683c63d146412d502928b079309e5951f24887c6
Daniel Rojo <danielrojo@gmail.com>
2023-04-18 11:37:04 +0200

    openapi documentation added with springdoc-openapi-starter-webmvc-ui

--- src/main/java/com/example/examples/services/BooksService.java
+++ src/main/java/com/example/examples/services/BooksService.java
@@ -28,7 +28,7 @@ public class BooksService {
     public BookEntity creaBook(BookRequest book) {
         // check that isbn is unique and not null
         if (this.booksRepository.findByIsbn(book.getIsbn()) != null || book == null) {
-            throw new Error400IsbnException(book.getIsbn());
+            throw new ErrorBooks400IsbnException(book.getIsbn());
         }
         return this.booksRepository.save(new BookEntity(book));
     }
@@ -38,7 +38,7 @@ public class BooksService {
         if (this.booksRepository.findByIsbn(book.getIsbn()) != null) {
             return this.booksRepository.save(book);
         }
-        throw new Error409Exception(book.getIsbn());
+        throw new ErrorBooks409Exception(book.getIsbn());
     }
 
     public BookEntity deleteBook(long id) {
@@ -47,41 +47,41 @@ public class BooksService {
             this.booksRepository.delete(book);
             return book;
         }
-        throw new Error404Exception(id);
+        throw new ErrorBooks404Exception(id);
     }
 
 }
 
-class Error404Exception extends RuntimeException {
-    public Error404Exception(long id) {
+class ErrorBooks404Exception extends RuntimeException {
+    public ErrorBooks404Exception(long id) {
         super("Book not found with id: " + id);
     }
 
-    public Error404Exception(String email) {
+    public ErrorBooks404Exception(String email) {
         super("Book not found with email: " + email);
     }
 }
 
-class Error400EmailException extends RuntimeException {
-    public Error400EmailException(String email) {
+class ErrorBooks400EmailException extends RuntimeException {
+    public ErrorBooks400EmailException(String email) {
         super("Book not found with email: " + email);
     }
 }
 
-class Error400NameException extends RuntimeException {
-    public Error400NameException(String name) {
+class ErrorBooks400NameException extends RuntimeException {
+    public ErrorBooks400NameException(String name) {
         super("Book not found with name: " + name);
     }
 }
 
-class Error409Exception extends RuntimeException {
-    public Error409Exception(String isbn) {
+class ErrorBooks409Exception extends RuntimeException {
+    public ErrorBooks409Exception(String isbn) {
         super("Book already exists with isbn: " + isbn);
     }
 }
 
-class Error400IsbnException extends RuntimeException {
-    public Error400IsbnException(String isbn) {
+class ErrorBooks400IsbnException extends RuntimeException {
+    public ErrorBooks400IsbnException(String isbn) {
         super("Book already exists with isbn: " + isbn);
     }
 }
